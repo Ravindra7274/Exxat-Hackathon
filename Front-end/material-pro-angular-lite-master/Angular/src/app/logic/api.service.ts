2d5059a26c7319977970e360f739db33c8fbf6f4
Shaquib Bharucha <shaquib.bharucha@EXXATSYSTEMS.COM>
2019-02-03 11:54:01 +0530

    calling api in component

--- Front-end/material-pro-angular-lite-master/Angular/src/app/logic/api.service.ts
+++ Front-end/material-pro-angular-lite-master/Angular/src/app/logic/api.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
-export class WorkoutService {
-  private headers: HttpHeaders;
-  private accessPointUrl: string = 'http://localhost:53877/api/workouts';
 
-  constructor(private http: HttpClient) {
-    this.headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
-  }
+export class ApiService {
+    private headers: HttpHeaders;
+    private accessPointUrl: string = 'http://localhost:53877/';
 
-get(ClientId:any,StudentName:any){
-    return this.http.get(this.accessPointUrl +'?ClientId=' + ClientId + '&StudentName=' + StudentName ,{headers:this.headers})
-}
+    constructor(private http: HttpClient) {
+        this.headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
+    }
+
+    getStudentList(ClientId: any, StudentName: any) {
+        return this.http.get(this.accessPointUrl + '' +'?ClientId=' + ClientId + '&StudentName=' + StudentName, { headers: this.headers });
+    }
+
+    getModules(){
+        return this.http.get(this.accessPointUrl+'Module',{headers:this.headers});
+    }
+
+    getCommonQueries(moduleName:any){
+        return this.http.get(this.accessPointUrl + '' + '?moduleName=' +moduleName , {headers:this.headers});
+    }
 }
